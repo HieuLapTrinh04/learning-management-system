e25e39e16495e41cedf18fcc51193d0ea0b433c8
HieuLapTrinh04 <buihieu11012004@gmail.com>
2026-06-28 22:52:24 +0700

    fix: add Upstash TLS compatibility

--- pkg/db/redis.go
+++ pkg/db/redis.go
@@ -4,7 +4,9 @@ import (
 	"github.com/HieuLapTrinh04/learning-management-system/pkg/logger"
 
 	"context"
+	"crypto/tls"
 	"fmt"
+	"strings"
 
 	"github.com/HieuLapTrinh04/learning-management-system/internal/config"
 	"github.com/redis/go-redis/v9"
@@ -12,11 +14,19 @@ import (
 
 // InitRedis initializes the go-redis client connection.
 func InitRedis(cfg *config.Config) *redis.Client {
-	rdb := redis.NewClient(&redis.Options{
+	opts := &redis.Options{
 		Addr:     fmt.Sprintf("%s:%s", cfg.RedisHost, cfg.RedisPort),
 		Password: cfg.RedisPassword,
 		DB:       0, // Use default DB
-	})
+	}
+
+	if strings.Contains(cfg.RedisHost, "upstash.io") {
+		opts.TLSConfig = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		}
+	}
+
+	rdb := redis.NewClient(opts)
 
 	// Test connection using Ping
 	ctx := context.Background()
